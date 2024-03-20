b5446729e59d4b75b0e6ab13cd48d78d01f1df1a
alamindev <mdalamin6554@gmail.com>
2024-03-20 23:18:14 +0600

    video change

--- src/components/Parking/Home/VideoArea.tsx
+++ src/components/Parking/Home/VideoArea.tsx
@@ -1,51 +1,36 @@
 "use client";
 import MenuLink from "@/components/Shared/MenuLink";
-import videoImg from "@/public/images/parking/home/parking-aparcamiento.png";
 import Image from "next/image";
 import { useState } from "react";
+import ModalVideo from "react-modal-video";
+import "react-modal-video/scss/modal-video.scss";
 
 export default function VideoArea() {
-  let [isVideo, setIsVideo] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   return (
-    <section className="relative  xl:min-h-screen">
+    <section className="relative min-h-[225px] sm:min-h-[450px] lg:min-h-[680px] xl:min-h-screen  ">
       <MenuLink />
-      {isVideo ? (
-        <div className="vimeo-wrapper">
-          <iframe
-            className="aspect-video w-screen  "
-            src="https://player.vimeo.com/video/347119375?background=1&autoplay=1&loop=1&byline=0&title=0"
-          ></iframe>
-        </div>
-      ) : (
-        <div className="min-h-[450px]">
-          <Image
-            src={videoImg}
-            className="object-cover object-center"
-            alt={"video imag"}
-            fill
-          />
 
-          <button
-            onClick={() => setIsVideo(true)}
-            type="button"
-            className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 "
-          >
-            <svg
-              className="w-[190px] xl:w-auto h-[190px] xl:h-auto"
-              width="305"
-              height="305"
-              viewBox="0 0 305 305"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M152.5 0C68.2438 0 0 68.2438 0 152.5C0 236.756 68.2438 305 152.5 305C236.756 305 305 236.756 305 152.5C305 68.2438 236.756 0 152.5 0ZM114.375 76.25L228.75 152.5L114.375 228.75V76.25Z"
-                fill="#EB6332"
-              />
-            </svg>
-          </button>
-        </div>
-      )}
+      <iframe
+        className="aspect-video w-screen "
+        src="https://player.vimeo.com/video/925183921?autoplay=1&muted=1&controls=0&background=1&loop=1&byline=0&title=0"
+      ></iframe>
+      <button onClick={() => setOpen(true)}>
+        <Image
+          alt="play"
+          src={"/icons/play.svg"}
+          className="w-20 md:w-[190px] xl:w-auto h-20 md:h-[190px] xl:h-auto absolute left-1/2 -translate-x-1/2 top-[40%] -translate-y-1/2 "
+          width={250}
+          height={250}
+        />
+      </button>
+
+      <ModalVideo
+        channel="vimeo"
+        isOpen={isOpen}
+        videoId={"904053646"}
+        onClose={() => setOpen(false)}
+      />
     </section>
   );
 }
--- src/components/Playa/Hero.jsx
+++ src/components/Playa/Hero.jsx
@@ -7,37 +7,25 @@ import "react-modal-video/scss/modal-video.scss";
 export default function Hero() {
   const [isOpen, setOpen] = useState(false);
   return (
-    <section className="relative min-h-[225px] sm:min-h-[450px] lg:min-h-[680px] xl:min-h-screen flex justify-center items-center">
-      <Image
-        alt="Playa"
-        src={"/playa/hero-playa.png"}
-        layout="cover"
-        className="hidden sm:block object-cover w-full "
-        fill
-      />
-      <Image
-        alt="Playa"
-        src={"/playa/hero-playa-mobile.png"}
-        layout="cover"
-        className="block sm:hidden object-cover w-full h-full"
-        fill
-      />
-      <div className="relative">
-        <button onClick={() => setOpen(true)}>
-          <Image
-            alt="play"
-            src={"/icons/play.svg"}
-            className="  h-[196px] w-[196px] sm:h-[305px] sm:w-[305px]"
-            width={305}
-            height={305}
-          />
-        </button>
-      </div>
+    <section className="relative min-h-[225px] sm:min-h-[450px] lg:min-h-[680px] xl:min-h-screen  ">
+      <iframe
+        className="aspect-video w-screen "
+        src="https://player.vimeo.com/video/925183921?autoplay=1&muted=1&controls=0&background=1&loop=1&byline=0&title=0"
+      ></iframe>
+      <button onClick={() => setOpen(true)}>
+        <Image
+          alt="play"
+          src={"/icons/play.svg"}
+          className="w-20 md:w-[190px] xl:w-auto h-20 md:h-[190px] xl:h-auto absolute left-1/2 -translate-x-1/2 top-[40%] -translate-y-1/2 "
+          width={250}
+          height={250}
+        />
+      </button>
 
       <ModalVideo
         channel="vimeo"
         isOpen={isOpen}
-        videoId={"904049593"}
+        videoId={"925183921"}
         onClose={() => setOpen(false)}
       />
     </section>
